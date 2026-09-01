68b0db2db226ccd7e384c7a51dadb3d1d39ba5c6
Dieg0g013 <ogeidtc07@gmail.com>
2026-08-31 22:49:48 -0600

    Add files via upload

---
+++ EVA1_1_SCOPE/src/eva1_1_scope/EVA1_1_SCOPE.java
@@ -0,0 +1,23 @@
+
+package eva1_1_scope;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class EVA1_1_SCOPE {
+   public static void main(String[] args) {
+      int x=100;//Existe dentro del bloque main
+      for (int i = 0; i < 10; i++){//Existe solo dentro del for
+         System.out.println("i = " + i);
+         x++;//Visible dentro del for
+      }
+      System.out.println("Valor final de la i = " + i);//Ya no existe
+      System.out.println("Valor final de la i = " + x);//Todavia existe
+      
+   }
+   public static void OtraFuncion(){
+      System.out.println("Valor final de x " + x);//No existe
+   }
+   
+}
