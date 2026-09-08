e55bdf0748e08cdd0b02596d923b799294afada8
Dieg0g013 <ogeidtc07@gmail.com>
2026-09-07 23:33:54 -0600

    Add files via upload

---
+++ EVA1_4_OBJETOS/src/eva1_4_objetos/EVA1_4_OBJETOS.java
@@ -0,0 +1,16 @@
+
+package eva1_4_objetos;
+public class EVA1_4_OBJETOS {
+   public static void main(String[] args) {
+      Prueba prueba = new Prueba();
+      System.out.println(prueba);
+      //ELIMINAR PRUEBA
+      //Terminar el programa --> garage collector (liberia memoria automaticamente)
+      //Eliminar "directamente" el objeto:
+      prueba = null;
+   }
+   
+}
+class Prueba{
+   
+}
