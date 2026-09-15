f07aa0473d0949c8d308ea2899246858399bc048
Dieg0g013 <ogeidtc07@gmail.com>
2026-09-15 15:45:41 -0600

    Add files via upload

---
+++ EVA1_10_4DIMENSIONES/src/eva1_10_4dimensiones/EVA1_10_4DIMENSIONES.java
@@ -0,0 +1,26 @@
+
+package eva1_10_4dimensiones;
+public class EVA1_10_4DIMENSIONES {
+   public static void main(String[] args) {
+      int [][][][] arreglo = new int [2][2][2][2];//almacena 16 enteros
+      for (int i = 0; i < arreglo.length; i++){//1er dimension
+         for (int j = 0; j < arreglo[i].length; j++){//2da 
+            for (int k = 0; k < arreglo[i][j].length; k++){//3ra
+               for (int l = 0; l < arreglo[i][j][k].length; l++){//4ta
+                  arreglo[i][j][k][l] = (int)(Math.random()*100);
+                  
+               }
+            }
+         }
+      }
+      for (int i = 0; i < arreglo.length; i++){//1er dimension
+         for (int j = 0; j < arreglo[i].length; j++){//2da 
+            for (int k = 0; k < arreglo[i][j].length; k++){//3ra
+               for (int l = 0; l < arreglo[i][j][k].length; l++){//4ta
+                  System.out.print("[" + arreglo[i][j][k][l] + "]");
+   }
+            }
+         }
+      }
+   }
+}
