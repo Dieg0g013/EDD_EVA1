dd23e6d85973c395f805ffd34ae15cf367a04dda
Dieg0g013 <ogeidtc07@gmail.com>
2026-09-16 21:22:46 -0600

    Add files via upload

---
+++ EVA1_11_DIMENSIONES/src/eva1_11_dimensiones/EVA1_11_DIMENSIONES.java
@@ -0,0 +1,28 @@
+
+package eva1_11_dimensiones;
+public class EVA1_11_DIMENSIONES {
+   public static void main(String[] args) {
+      // TODO code application logic here
+      int[][] matriz;
+      matriz = new int[3][];
+      //cada fila tendra diferente cantidad de columnas:
+      matriz[0] = new int[5];
+      matriz[1] = new int[10];
+      matriz[2] = new int[3];
+      
+      for(int i =0; i<matriz.length;i++){
+         for(int j =0; j<matriz[i].length; j++){
+            matriz[i][j] = (int)(Math.random()*100);
+         }
+      }
+      for(int i =0; i<matriz.length;i++){
+         for(int j =0; j<matriz[i].length; j++){
+            System.out.print("[" + matriz [i][j] + "]");
+         }
+         System.out.println("") ;     
+            }
+      
+      
+   }
+   
+}
